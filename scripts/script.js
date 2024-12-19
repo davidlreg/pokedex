78cc786aedad156f5562165225041a243749d148
davidlreg <davidgerliz02@gmail.com>
2024-12-19 22:32:31 +0100

    added total pokemon counter

--- scripts/script.js
+++ scripts/script.js
@@ -6,27 +6,29 @@ function init() {
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
 
 async function fetchPokedexData() {
-    try {
-        // Daten von der API abrufen
-        const response = await fetch(BASE_URL);
+  try {
+    // Daten von der API abrufen
+    const response = await fetch(BASE_URL);
 
-        // Fehler prüfen
-        if (!response.ok) {
-            throw new Error(`HTTP-Fehler! Status: ${response.status}`);
-        }
-
-        // JSON-Daten parsen
-        const data = await response.json();
-
-        // Ergebnis in der Konsole ausgeben
-        console.log({
-            count: data.count, // Gesamtanzahl der Pokemon
-            results: data.results // Liste der Pokemon mit Namen und URLs
-        });
-
-    } catch (error) {
-        // Fehler behandeln und ausgeben
-        console.error("Fehler beim Abrufen der Daten:", error);
+    // Fehler prüfen
+    if (!response.ok) {
+      throw new Error(`HTTP-Fehler! Status: ${response.status}`);
     }
-}
 
+    // JSON-Daten parsen
+    const data = await response.json();
+
+    // Gesamtanzahl der Pokemon im HTML anzeigen
+    let totalPokemonCountElement = document.getElementById("totalPokemonCount");
+    totalPokemonCountElement.innerHTML = `Total Pokemon Count: ${data.count}`;
+
+    // Ergebnis in der Konsole ausgeben
+    console.log({
+      count: data.count, // Gesamtanzahl der Pokemon
+      results: data.results, // Liste der Pokemon mit Namen und URLs
+    });
+  } catch (error) {
+    // Fehler behandeln und ausgeben
+    console.error("Fehler beim Abrufen der Daten:", error);
+  }
+}
