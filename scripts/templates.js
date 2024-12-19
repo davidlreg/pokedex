a1c59e7ba121b09bb111c9845b98ea568e41b66b
davidlreg <davidgerliz02@gmail.com>
2024-12-19 23:09:14 +0100

    added more styling to cards

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,6 +1,6 @@
 function pokemonCardTemplate(name, url) {
   return `
-     <div class="pokemon-card">
+     <div class="pokemonCard">
       <h2>${name}</h2>
       <a href="${url}" target="_blank">Details</a>
     </div>
