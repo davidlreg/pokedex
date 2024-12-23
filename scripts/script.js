d5ea3f86eb7e321c8b804816effc6a5a6573054a
davidlreg <davidgerliz02@gmail.com>
2024-12-23 16:22:50 +0100

    added functiopn for rendering pokemon picture in card

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,32 @@
-function init() {
-  console.log("Pokedex geladen!");
-  fetchPokedexData();
-}
-
 let pokemonStartCount = 1;
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+const typeToIcon = {
+  bug: "./assets/img/pokemonTypes/bug.svg",
+  dark: "./assets/img/pokemonTypes/dark.svg",
+  dragon: "./assets/img/pokemonTypes/dragon.svg",
+  electric: "./assets/img/pokemonTypes/electric.svg",
+  fairy: "./assets/img/pokemonTypes/fairy.svg",
+  fighting: "./assets/img/pokemonTypes/fighting.svg",
+  fire: "./assets/img/pokemonTypes/fire.svg",
+  flying: "./assets/img/pokemonTypes/flying.svg",
+  ghost: "./assets/img/pokemonTypes/ghost.svg",
+  grass: "./assets/img/pokemonTypes/grass.svg",
+  ground: "./assets/img/pokemonTypes/ground.svg",
+  ice: "./assets/img/pokemonTypes/ice.svg",
+  normal: "./assets/img/pokemonTypes/normal.svg",
+  poison: "./assets/img/pokemonTypes/poison.svg",
+  psychic: "./assets/img/pokemonTypes/psychic.svg",
+  rock: "./assets/img/pokemonTypes/rock.svg",
+  steel: "./assets/img/pokemonTypes/steel.svg",
+  water: "./assets/img/pokemonTypes/water.svg",
+};
+
+function init() {
+  fetchPokedexData();
+  console.log("Pokedex geladen!");
+}
 
 async function fetchPokedexData() {
   // Daten von der API abrufen
@@ -35,11 +56,6 @@ async function renderPokemonCards(pokemonList) {
   let container = document.getElementById("content");
   let pokemonCounter = 1;
 
-  if (!container) {
-    console.error("Element mit ID 'content' nicht gefunden.");
-    return;
-  }
-
   container.classList.add("pokemonContainer");
 
   for (let i = 0; i < 25 && i < pokemonList.length; i++) {
@@ -53,43 +69,24 @@ async function renderPokemonCards(pokemonList) {
     );
 
     // Typen abrufen und in der Konsole ausgeben
-    await fetchPokemonTypes(pokemon.url, pokemonCounter);
+    await fetchPokemonDetails(pokemon.url, pokemonCounter);
 
     pokemonCounter++;
   }
 }
 
-async function fetchPokemonTypes(pokemonUrl, cardId) {
-  const responsePokemonTypes = await fetch(pokemonUrl);
+async function fetchPokemonDetails(pokemonUrl, cardId) {
+  const responsePokemonDetails = await fetch(pokemonUrl);
 
-  const pokemonTypeData = await responsePokemonTypes.json();
-  const types = pokemonTypeData.types.map((type) => type.type.name);
+  const pokemonDetails = await responsePokemonDetails.json();
+  const types = pokemonDetails.types.map((type) => type.type.name);
+  const pokemonImg = pokemonDetails.sprites.front_default;
+
   insertTypeIcons(types, cardId);
-  // console.log(`Pokemon: ${pokemonTypeData.name}, Types: ${types.join(", ")}`);
+  insertPokemonImage(pokemonImg, cardId);
 }
 
 function insertTypeIcons(types, cardId) {
-  const typeToIcon = {
-    bug: "./assets/img/pokemonTypes/bug.svg",
-    dark: "./assets/img/pokemonTypes/dark.svg",
-    dragon: "./assets/img/pokemonTypes/dragon.svg",
-    electric: "./assets/img/pokemonTypes/electric.svg",
-    fairy: "./assets/img/pokemonTypes/fairy.svg",
-    fighting: "./assets/img/pokemonTypes/fighting.svg",
-    fire: "./assets/img/pokemonTypes/fire.svg",
-    flying: "./assets/img/pokemonTypes/flying.svg",
-    ghost: "./assets/img/pokemonTypes/ghost.svg",
-    grass: "./assets/img/pokemonTypes/grass.svg",
-    ground: "./assets/img/pokemonTypes/ground.svg",
-    ice: "./assets/img/pokemonTypes/ice.svg",
-    normal: "./assets/img/pokemonTypes/normal.svg",
-    poison: "./assets/img/pokemonTypes/poison.svg",
-    psychic: "./assets/img/pokemonTypes/psychic.svg",
-    rock: "./assets/img/pokemonTypes/rock.svg",
-    steel: "./assets/img/pokemonTypes/steel.svg",
-    water: "./assets/img/pokemonTypes/water.svg",
-  };
-
   const typeContainer = document.querySelector(`#types-${cardId}`);
 
   if (!typeContainer) {
@@ -105,4 +102,20 @@ function insertTypeIcons(types, cardId) {
       console.warn(`Kein Symbol für Typ "${type}" gefunden.`);
     }
   });
+}
+
+function insertPokemonImage(imageUrl, cardId) {
+  const pokemonPictureContainer = document.querySelector(`#picture-${cardId}`);
+
+  if (!pokemonPictureContainer) {
+    console.error(`Container für Typen nicht gefunden: #types-${cardId}`);
+    return;
+  }
+
+  if (imageUrl) {
+    pokemonPictureContainer.innerHTML = `<img src="${imageUrl}" alt="Pokemon Image" class="pokemon-img">`;
+  } else {
+    console.warn(`Kein Bild für Karte ${cardId} verfügbar.`);
+    pokemonPictureContainer.innerHTML = `<p>Bild nicht verfügbar</p>`;
+  }
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,8 @@
-function pokemonCardTemplate(number, name, url) {
+function pokemonCardTemplate(number, name) {
   return `
      <div class="pokemonCard">
       <h2><span>#${number}</span> ${name}</h2>
+      <div class="pokemonImg" id="picture-${number}"></div>
       <div class="types" id="types-${number}"></div>
     </div>
     `;
