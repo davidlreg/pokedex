7afaa608f88a2cb8d8160dbf5d05882be395c191
davidlreg <davidgerliz02@gmail.com>
2024-12-24 10:12:21 +0100

    added text

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,7 @@
 function pokemonCardTemplate(number, name) {
   return `
      <div class="pokemonCard">
-      <h2><span>#${number}</span> ${name}</h2>
+      <h2><span>Nr.${number}</span> ${name}</h2>
       <div class="pokemonImg" id="picture-${number}"></div>
       <div class="types" id="types-${number}"></div>
     </div>
